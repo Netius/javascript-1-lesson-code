75b5e3e7a33d65250ef77bbadb6d2040ffaa6cda
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2020-03-20 12:54:10 +0100

    Finished Step-2

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const container = document.querySelector(".container");
 // newHTML's value will change so we use let to declare it
 let newHTML = "";
 
+
+
 for (let i = 0; i < actionGames.length; i++) {
     // create a default value for the rating
     let ratingValue = "Not rated";
@@ -13,6 +15,14 @@ for (let i = 0; i < actionGames.length; i++) {
         ratingValue = actionGames[i].rating;
     }
 
+    //setting up genres of each game
+    const genres = actionGames[i].genres;
+    const genresHTML = makeGenres(genres);
+
+    //passing platfomrs in function
+    const platforms = actionGames[i].platforms;
+    const platformsHTML = makePlatforms(platforms);
+
     // each time the loop is executed, assign the details variable a template literal
     // this is a string with embedded variables
     const details = `<div class="card">
@@ -20,6 +30,8 @@ for (let i = 0; i < actionGames.length; i++) {
                         <div class="details">
                             <h4 class="name">${actionGames[i].name}</h4>
                             <div class="rating">${ratingValue}</div>
+                            ${genresHTML}
+                            <div class="platforms">${platformsHTML}</div>
                         </div>
                     </div>`;
     // add the value of details to the existing value of newHTML
@@ -28,3 +40,25 @@ for (let i = 0; i < actionGames.length; i++) {
 
 // set container's innerHTML property to be the HTML that was created in the loop
 container.innerHTML = newHTML;
+
+
+//setting up genres 
+function makeGenres(genreArray) {
+    let genreHTML = "";
+    for (let i = 0; i < genreArray.length; i++) {
+        genreHTML += `<a class="genre">${genreArray[i].name}</a>`;
+    }
+    return genreHTML;
+
+}
+
+
+//function for platform array
+function makePlatforms(platformsArray) {
+    let platformsHTML = "";
+    for (let i = 0; i < platformsArray.length; i++) {
+        platformsHTML += `<span>${platformsArray[i].platform.name}</span>`;
+
+    }
+    return platformsHTML;
+}
