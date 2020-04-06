f513d7e994ff820f1a320617fc52ea4c16683821
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2020-04-06 22:32:25 +0200

    Finished Module 2

--- js/script.js
+++ js/script.js
@@ -1,55 +1,78 @@
-function loadGames() {
-    const container = document.querySelector(".container.results");
-    let newHTML = "";
+function loadGames(event) {
+	const buttonsId = event.target.id;
 
-    for (let i = 0; i < actionGames.length; i++) {
+	let arrayToLoopThrough = [];
 
-        let ratingValue = "Not rated";
+	switch (buttonsId) {
+		case "action":
+			arrayToLoopThrough = actionGames;
+			break;
+		case "shooter":
+			arrayToLoopThrough = shooterGames;
+			break;
+		case "rpg":
+			arrayToLoopThrough = rpgGames;
+			break;
+		default:
+            alert("No such thing!")
+	}
 
-        if (actionGames[i].rating) {
-            ratingValue = actionGames[i].rating;
-        }
+	console.log(arrayToLoopThrough);
 
-        const genres = actionGames[i].genres;
-        const genresHTML = makeGenres(genres);
+	const container = document.querySelector(".container.results");
+	let newHTML = "";
 
-        const platforms = actionGames[i].platforms;
-        const platformsHTML = makePlatforms(platforms);
+	for (let i = 0; i < arrayToLoopThrough.length; i++) {
+		let ratingValue = "Not rated";
 
-        const details = `<div class="card">
-                            <div class="image" style="background-image: url(${actionGames[i].background_image});"></div>
+		if (arrayToLoopThrough[i].rating) {
+			ratingValue = arrayToLoopThrough[i].rating;
+		}
+
+		const genres = arrayToLoopThrough[i].genres;
+		const genresHTML = makeGenres(genres);
+
+		const platforms = arrayToLoopThrough[i].platforms;
+		const platformsHTML = makePlatforms(platforms);
+
+		const details = `<div class="card">
+                            <div class="image" style="background-image: url(${arrayToLoopThrough[i].background_image});"></div>
                             <div class="details">
-                                <h4 class="name">${actionGames[i].name}</h4>
+                                <h4 class="name">${arrayToLoopThrough[i].name}</h4>
                                 <div class="rating">${ratingValue}</div>
                                 ${genresHTML}
                                 <div class="platforms">${platformsHTML}</div>
                             </div>
                         </div>`;
 
-        newHTML += details;
-    }
+		newHTML += details;
+	}
 
-    container.innerHTML = newHTML;
+	container.innerHTML = newHTML;
 }
 
 function makeGenres(genreArray) {
+	let genreHTML = "";
 
-    let genreHTML = "";
+	for (let i = 0; i < genreArray.length; i++) {
+		genreHTML += `<a class="genre">${genreArray[i].name}</a>`;
+	}
 
-    for (let i = 0; i < genreArray.length; i++) {
-        genreHTML += `<a class="genre">${genreArray[i].name}</a>`;
-    }
-
-    return genreHTML;
+	return genreHTML;
 }
 
 function makePlatforms(platformsArray) {
+	let platformsHTML = "";
 
-    let platformsHTML = "";
+	for (let i = 0; i < platformsArray.length; i++) {
+		platformsHTML += `<span>${platformsArray[i].platform.name}</span>`;
+	}
 
-    for (let i = 0; i < platformsArray.length; i++) {
-        platformsHTML += `<span>${platformsArray[i].platform.name}</span>`;
-    }
+	return platformsHTML;
+}
 
-    return platformsHTML;
+const buttons = document.querySelectorAll(".btn.btn-secondary");
+
+for (let i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener("click", loadGames);
 }
