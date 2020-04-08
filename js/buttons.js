981b707d0992fe3912e39c3b5654d69244dc0f40
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2020-04-08 13:02:10 +0200

    Finished Module 3

--- js/buttons.js
+++ js/buttons.js
@@ -1,5 +1,16 @@
 const buttons = document.querySelectorAll(".btn.btn-secondary");
 
-for(let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", loadGames);
+for (let i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener("click", loadGames);
+	buttons[i].addEventListener("click", handleActiveClass);
+}
+
+function handleActiveClass(event) {
+	for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove("active");
+        console.log(buttons[i]);
+	}
+
+	event.target.classList.add("active");
+	console.log(event.target);
 }
