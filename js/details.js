182eb1df54ef5d7536bedd20a1a72fb2f8ec307d
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2020-04-27 15:23:10 +0200

    finished Lesson 4

--- js/details.js
+++ js/details.js
@@ -29,4 +29,45 @@ fetch(detailsUrl)
 
 function createDetails(details) {
     console.dir(details);
+    const container = document.querySelector(".container");
+    const loader = document.querySelector(".loader");
+    container.removeChild(loader);
+
+    const heading = document.createElement("h1");
+    heading.innerText = details.name;
+    container.appendChild(heading);
+
+    console.log(heading);
+
+
+    const backgroundImage = document.createElement("div");
+    backgroundImage.className = "details-image";
+    backgroundImage.style.backgroundImage = `url("${details.background_image}")`
+    container.appendChild(backgroundImage);
+
+    const description = document.createElement("div");
+    description.className = "details-description";
+    // we'll innerHTML as description contains HTML
+    description.innerHTML = details.description;
+
+    container.appendChild(description);
+
+
+    const releaseDate = document.createElement("time");
+    releaseDate.className = "details-date";
+    releaseDate.innerText = `Released: ${details.released}`;
+
+    description.before(releaseDate);
+
+    const genres = document.createElement("div");
+    genres.className = "details-genres";
+
+    genres.innerHTML = makeGenres(details.genres);
+
+    container.appendChild(genres);
+
+
+
+    document.title = details.name + " | " + document.title;
+
 }
